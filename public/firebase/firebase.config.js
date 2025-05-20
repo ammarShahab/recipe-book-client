9615e20696c0a32092800277219a86969a5a1a28
Ashahab007 <ashahab007@gmail.com>
2025-05-21 03:44:39 +0600

    use env variables for firebase config keys

--- public/firebase/firebase.config.js
+++ public/firebase/firebase.config.js
@@ -4,13 +4,23 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQi3Yr9imUkv0_u_w5inrSIXUqMKBCjrw",
-  authDomain: "b11a10-client-side-ashahab007.firebaseapp.com",
-  projectId: "b11a10-client-side-ashahab007",
-  storageBucket: "b11a10-client-side-ashahab007.firebasestorage.app",
-  messagingSenderId: "850718892573",
-  appId: "1:850718892573:web:c6d040214506e96157dcf4",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+// .env.local file
+/* 
+VITE_apiKey=AIzaSyDQi3Yr9imUkv0_u_w5inrSIXUqMKBCjrw
+VITE_authDomain=b11a10-client-side-ashahab007.firebaseapp.com
+VITE_projectId=b11a10-client-side-ashahab007
+VITE_storageBucket=b11a10-client-side-ashahab007.firebasestorage.app
+VITE_messagingSenderId=850718892573
+VITE_appId=1:850718892573:web:c6d040214506e96157dcf4 
+*/
