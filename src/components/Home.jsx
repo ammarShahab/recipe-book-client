5b0fe7be15375ca1988272cb4aa45c13cea6cae0
Ashahab007 <ashahab007@gmail.com>
2025-05-20 23:17:45 +0600

    added react counter app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import RecipeHighlights from "./RecipeHighlights";
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
+      <RecipeHighlights></RecipeHighlights>
     </>
   );
 };
