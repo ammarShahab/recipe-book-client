2bb21e44665b61dbc45968879cb9efb02547c9ba
Ashahab007 <ashahab007@gmail.com>
2025-05-20 23:50:42 +0600

    added newsletter section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import RecipeHighlights from "./RecipeHighlights";
+import NewsLetter from "./NewsLetter";
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
       <RecipeHighlights></RecipeHighlights>
+      <NewsLetter></NewsLetter>
     </>
   );
 };
